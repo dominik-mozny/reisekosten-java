98c2cffc5afd0aab3d01c782df67c6335cb22561
Dominik Mozny <dominik.mozny@sbb.ch>
2022-06-20 15:01:06 +0200

    kjkjkj

--- domain/src/test/java/domain/SmokeTest.java
+++ domain/src/test/java/domain/SmokeTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -112,5 +111,4 @@ public class SmokeTest {
             accountingService.accept(businessTravel, now);
         });
     }
-
 }
